3b513b85f5f8c2fd853d6cb567068febe184cdce
PC\Windows 18 <canh.pham@bmbsoft.com.vn>
2021-01-26 21:09:54 +0700

    build structure

---
+++ ChatAppBE/src/main/java/Web/ChatAppBE/ChatAppBeApplication.java
@@ -0,0 +1,13 @@
+package Web.ChatAppBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppBeApplication.class, args);
+	}
+
+}
